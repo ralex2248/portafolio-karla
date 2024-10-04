2fc685c2496da78de82d09b32206bbbb2e05ef98
ralex2248 <90098814+ralex2248@users.noreply.github.com>
2024-10-04 17:25:22 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+const toggleTheme = document.getElementById("toggle-theme");
+const toggleIcon = document.getElementById("toggle-icon");
+const toggleText = document.getElementById("toggle-text");
+
+const toggleColors = document.getElementById("toggle-colors");
+
+const rootStyles = document.documentElement.style;
+
+toggleTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (toggleIcon.src.includes("moon.svg")) {
+    toggleIcon.src = "assets/icons/sun.svg";
+    toggleText.textContent = "tema claro";
+  } else {
+    toggleIcon.src = "assets/icons/moon.svg";
+    toggleText.textContent = "tema oscuro";
+  }
+});
+
+toggleColors.addEventListener("click", (e) => {
+  rootStyles.setProperty("--primary-color", e.target.dataset.color);
+});
+
+
+
